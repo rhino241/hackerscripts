9b5376d705852808164a1d78414e46e7bdddeda2
Rhino241 <146070840+rhino241@users.noreply.github.com>
2024-07-02 19:10:49 -0400

    Update proxy.py

--- hackerscripts-main/proxy.py
+++ hackerscripts-main/proxy.py
@@ -7,7 +7,7 @@ def save_content_from_links(links, destination_file):
             content = response.text
             file.write(content)
 
-    print(f"Content saved from {len(links)} links to {destination_file}. coded by FrostedFlakes666")
+    print(f"Content saved from {len(links)} links to {destination_file}.")
 
 
 links = ['https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt', 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt', 'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',' https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all','https://www.proxy-list.download/api/v1/get?type=http','https://raw.githubusercontent.com/saisuiu/uiu/main/free.txt','https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt']
